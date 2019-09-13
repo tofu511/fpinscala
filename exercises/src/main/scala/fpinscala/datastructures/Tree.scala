e37e8cda42609eea01c8492cc9fcf363b8a96aa6
tofu511 <mokmok511@gmail.com>
2019-09-13 23:43:21 +0900

    update Tree

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -27,6 +27,30 @@ object Tree {
     case Branch(l,r) => Branch(map(l)(f), map(r)(f))
   }
 
+  def size2[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(left, right) => size2(left) + size2(right)
+  }
 
+  def maximum2(t: Tree[Int]): Int = t match {
+    case Leaf(value) => value
+    case Branch(l, r) => maximum2(l) max maximum2(r)
+  }
+
+  def depth2[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(left, right) => 1 + (depth2(left) max depth2(right))
+  }
+
+  def map2[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(value) => Leaf(f(value))
+    case Branch(left, right) => Branch(map2(left)(f), map2(right)(f))
+  }
+
+  // 抽象的すぎてよくわからん
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(value) => f(value)
+    case Branch(left, right) => g(fold(left)(f)(g), fold(right)(f)(g))
+  }
 
 }
--- exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -1,5 +1,37 @@
 package fpinscala.datastructures
 
-class TreeTest {
+import org.scalatest.{FunSpec, Matchers}
 
+class TreeSpec extends FunSpec with Matchers {
+
+  val t = Branch(Branch(Leaf(100), Leaf(20)), Leaf(3))
+
+  describe("Tree") {
+    describe("size2") {
+      it("should return node's size") {
+        Tree.size2(t) should be (3)
+        Tree.size2(Leaf(2)) should be (1)
+      }
+    }
+
+    describe("maximum2") {
+      it("should return maximum value") {
+        Tree.maximum2(t) should be (100)
+        Tree.maximum2(Leaf(5)) should be (5)
+      }
+    }
+
+    describe("depth2") {
+      it("should return deepest path") {
+        Tree.depth2(t) should be (2)
+        Tree.depth2(Leaf(5)) should be (0)
+      }
+    }
+
+    describe("map2") {
+      Tree.map2(t)(x => x + 1) should be (Branch(Branch(Leaf(101), Leaf(21)), Leaf(4)))
+      Tree.map2(t)(x => x * x) should be (Branch(Branch(Leaf(10000), Leaf(400)), Leaf(9)))
+    }
+
+  }
 }
